24d1d3f7b06730dcbefe4803da35bee5c835765b
Srdjan Simic <srdjansimic@gmail.com>
2018-12-30 22:29:37 +0100

    Add project files.

---
+++ FolderPowerTools/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderPowerTools
+{
+    public class Class1
+    {
+    }
+}
