5a1dc6910dc84eed3b4c76b4c52a359e36003924
Srdjan Simic <srdjansimic@gmail.com>
2018-12-30 23:11:48 +0100

    Code added and an icon

--- FolderPowerTools/CreateDateFolder.cs
+++ FolderPowerTools/CreateDateFolder.cs
@@ -1,18 +1,68 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using SharpShell.Attributes;
 using SharpShell.SharpContextMenu;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
 
 namespace FolderPowerTools
 {
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.Class, @"Directory\Background")]
     public class CreateDateFolder : SharpContextMenu
     {
         protected override bool CanShowMenu()
         {
-            throw new System.NotImplementedException();
+            return true;
         }
 
         protected override ContextMenuStrip CreateMenu()
         {
-            throw new System.NotImplementedException();
+            //  Create the context menu.
+            var contextMenu = new ContextMenuStrip();
+
+            //  Add the 'copy path' item. This just copies the folder path for the extension.
+            var menuItem = new ToolStripMenuItem
+            {
+                Text = @"Create Date Folder",
+                Image = Properties.Resources.Add_Folder_icon
+            };
+
+            // Ovo kopira folder u Clipboard
+            //menuItem.Click += (sender, args) => Clipboard.SetText(FolderPath);
+            menuItem.Click += (sender, args) => CreateFolder();
+            contextMenu.Items.Add(menuItem);
+
+            //  Return the menu.
+            return contextMenu;
+        }
+        private string GetCurrentDate()
+        {
+            var currentDate = DateTime.Today;
+            return currentDate.ToString("yyyy-MM-dd");
+        }
+        private void CreateFolder()
+        {
+            var currentDate = GetCurrentDate();
+            var currentPath = "";
+            if (FolderPath != null)
+            {
+                currentPath = FolderPath;
+            }
+
+            var pathString = System.IO.Path.Combine(currentPath, currentDate);
+            Clipboard.SetText(pathString);
+
+            try
+            {
+                System.IO.Directory.CreateDirectory(pathString);
+            }
+            catch (Exception exception)
+            {
+                var builder = new StringBuilder();
+                builder.AppendLine($"Greska prilikom kreiranja foldera {pathString}: {exception.Message}");
+                //MessageBox();
+            }
         }
     }
 }
