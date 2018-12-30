979b4e6102f98a11775e7669afbbba31f95086b0
Srdjan Simic <srdjansimic@gmail.com>
2018-12-30 22:45:55 +0100

    ShellExtensions initial commit

---
+++ FolderPowerTools/CreateDateFolder.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using SharpShell.SharpContextMenu;
+
+namespace FolderPowerTools
+{
+    public class CreateDateFolder : SharpContextMenu
+    {
+        protected override bool CanShowMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
